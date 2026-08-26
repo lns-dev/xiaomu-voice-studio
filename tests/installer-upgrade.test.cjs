5591bff95a783f16f9ba1d9cfdb9f325c2193022
lns-dev <lyh20030730@outlook.com>
2026-08-26 11:47:48 +0800

    Revert "fix: prevent installer icon clipping at high DPI"
    This reverts commit 0e54b5f01dc32892e5a2f2408150e3b631dea8ac.

--- tests/installer-upgrade.test.cjs
+++ tests/installer-upgrade.test.cjs
@@ -11,13 +11,3 @@ test('overwrite installer preserves all user-owned software-root directories', (
   }
   assert.match(installer, /Function \.onGUIEnd[\s\S]*Call restoreXiaoMuUpgradeData/);
 });
-
-test('installer uses a DPI-safe dedicated icon without changing the installed app icon', () => {
-  const installer = fs.readFileSync(path.join(__dirname, '..', 'build', 'installer.nsh'), 'utf8');
-  const builder = fs.readFileSync(path.join(__dirname, '..', 'electron-builder.yml'), 'utf8');
-  assert.match(installer, /ManifestDPIAware true/);
-  assert.match(builder, /installerIcon: build\/installer-icon\.ico/);
-  assert.match(builder, /uninstallerIcon: build\/installer-icon\.ico/);
-  assert.match(builder, /installerHeaderIcon: build\/installer-icon\.ico/);
-  assert.match(builder, /win:[\s\S]*icon: build\/icon\.ico/);
-});
